e1256d20f69c109dd533f87364bfff7599d2b09c
CodinGitHub <gms2programador@gmail.com>
2022-09-02 03:49:19 -0500

    Manejo del DOM

--- dom.js
+++ dom.js
@@ -1,7 +1,29 @@
-// let titulo = document.getElementById('title')
-// let titulo = document.getElementsByClassName('encabezado')
-let titulo = document.getElementsByTagName('h1')
+//ACCEDER A ELEMENTOS
+let tituloId = document.getElementById('title');
+// let tituloClass = document.getElementsByClassName('encabezado');
+// let tituloTag = document.getElementsByTagName('h2');
 
-console.log(titulo)
+// let tituloId2 = document.querySelector('#title')
+// let tituloClass2 = document.querySelectorAll('.encabezado')
+// let tituloTag2 = document.querySelectorAll('h2')
 
-titulo[0].style.color = 'red';
+//ESCUCHAR EVENTOS
+let boton = document.getElementsByTagName('button')
+
+boton[0].addEventListener('click', ()=>{
+    //CAMBIAR ESTILOS
+    tituloId.style.color = 'yellow';
+    tituloId.style.width = '250px';
+    tituloId.style.backgroundColor = 'black';
+    tituloId.style.textAlign = 'left';
+});
+
+// IMPRIMIR UN NOMBRE INGRESADO POR INPUT EN PANTALLA
+let enviarBtn = document.querySelector('#enviarBtn');
+
+enviarBtn.addEventListener('click', ()=>{
+    // Ver lo que hay en el input
+    let input = document.querySelector('#input')
+    console.log(input.value)
+    // Imprimir/Mostrar en la pagina
+})
